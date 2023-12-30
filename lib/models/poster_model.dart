1fa8b00aa01928b3d5eb01102605aa9e49ae4dc9
MACPU1998 <Mahdiehr.rahbar@gmail.com>
2023-12-30 12:16:58 +0330

    create model

---
+++ lib/models/poster_model.dart
@@ -0,0 +1,18 @@
+
+class PosterModel {
+  String? id;
+  String? title;
+  String? image;
+
+  PosterModel({
+    this.id,
+    this.title,
+    this.image,
+  });
+
+  PosterModel.fromJson(Map<String, dynamic> element) {
+    id = element["id"];
+    title = element["title"];
+    image = /*ApiUrlConstant.hostDlUrl +*/ element["image"];
+  }
+}
